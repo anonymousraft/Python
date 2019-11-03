138c18d240863558bed958230bc4b7dfb52195e9
mandeephub <suggestions.live@gmail.com>
2019-11-03 21:52:22 +0530

    numbers program added

---
+++ Day1/numbers.py
@@ -0,0 +1,13 @@
+#This Programs print some numbers
+
+print(2)
+
+print(2.2)
+
+print(2+2)
+
+print(72/2)
+
+print(46)
+
+print('these  numbers are greater then 20:-  ' + '22, 23, 68' + ' (22, 23, 68 is string hahaha) ' )
