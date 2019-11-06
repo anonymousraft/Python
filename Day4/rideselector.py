b6b3029c3fdd3513ccc667a160ae33cede5911ff
mandeephub <suggestions.live@gmail.com>
2019-11-07 00:00:10 +0530

    day 4 code added

---
+++ Day4/rideselector.py
@@ -0,0 +1,18 @@
+#ride selector program
+flag = True
+while flag:
+    print('These are the available rides')
+    print('1: River ride')
+    print('2: Fire ride')
+    print('3: Ice ride')
+    print('4: Lava ride')
+    print('5: Grass ride')
+    ride = int(input('Select your ride\n'))
+    if(ride > 1 & ride < 5):
+        print('1: River ride')
+        print('2: Fire ride')
+        print('3: Ice ride')
+        print('4: Lava ride')
+        print('5: Grass ride')
+print('test')
+     
---
+++ Day4/rocketlaunch.py
@@ -0,0 +1,10 @@
+#rocket launch program
+import time
+print('Get Set for a Rocket launch')
+timer = 1
+while timer <= 10:
+    print(timer)
+    timer = timer+1
+    time.sleep(1)
+print('WE ARE OFF TO MOON')
+    
